a15ca0a8a35f47f983905e03e177ed2ed452553c
Jonathan Deleon <Jondeleon856@gmail.com>
2018-02-12 15:14:30 -0500

    added feature to handle resetting of table if current table exists

--- design.js
+++ design.js
@@ -1,4 +1,3 @@
-
 // Select color input
 const colorChosen = document.getElementById('colorPicker');
 colorChosen.addEventListener('change',getColor);
@@ -10,10 +9,10 @@ function getColor(){
   return newColor;
 }
 
+
 // Select size input
 const submitButton = document.getElementById('sbmit');
 submitButton.addEventListener('click',getFormData);
-
 
 function getFormData(event) {
   const gridHeight = document.getElementById('grid-height').value;
@@ -24,13 +23,17 @@ function getFormData(event) {
   event.preventDefault();
 }
 
+
 // When size is submitted by the user, call makeGrid()
 //* FUNCTION THAT CREATES GRID *
-
 function makeGrid(height,width) {
-
-
     const newCanvas = document.getElementById('pixelCanvas');
+
+    //Resets the table if a current grid is in place
+    if(newCanvas.hasChildNodes()){
+      newCanvas.removeChild(newCanvas.firstChild);
+    }
+
     for(let i = 0; i < height; i++){
       //Create a for loop to build the rows, and set the attribute for the row
       const row = newCanvas.insertRow();
